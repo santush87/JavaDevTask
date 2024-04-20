a225681e8232db8bae2a5a54c15cddbbf5d6e639
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-20 18:47:10 +0300

    Added final task for Excel reading

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.Tasks.ArrayListIterator;
-import org.example.Tasks.Duplicates;
-import org.example.Tasks.StringReversing;
-import org.example.Tasks.WordsCounter;
+import org.example.Tasks.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -14,37 +11,38 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 //        First
-        System.out.println("Enter string to reverse: ");
-        String reversed = StringReversing.reverseString(sc.nextLine());
-        System.out.println(reversed);
+//        System.out.println("Enter string to reverse: ");
+//        String reversed = StringReversing.reverseString(sc.nextLine());
+//        System.out.println(reversed);
 
 //        Second
-        System.out.println("Enter a text:");
-        String input2 = sc.nextLine();
-        int words = WordsCounter.countWords(input2);
-        System.out.println("Number of words: " + words);
+//        System.out.println("Enter a text:");
+//        String input2 = sc.nextLine();
+//        int words = WordsCounter.countWords(input2);
+//        System.out.println("Number of words: " + words);
 
 
 //        Third
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList.add("Apple");
-        arrayList.add("Banana");
-        arrayList.add("Orange");
-        arrayList.add("Mango");
-
-        System.out.println("Iterating over the ArrayList using for loop:");
-        ArrayListIterator.arrayListIterationUsingFor(arrayList);
-        System.out.println();
-        System.out.println("Iterating over the ArrayList using while loop:");
-        ArrayListIterator.arrayListIterationUsingWhile(arrayList);
+//        ArrayList<String> arrayList = new ArrayList<>();
+//        arrayList.add("Apple");
+//        arrayList.add("Banana");
+//        arrayList.add("Orange");
+//        arrayList.add("Mango");
+//
+//        System.out.println("Iterating over the ArrayList using for loop:");
+//        ArrayListIterator.arrayListIterationUsingFor(arrayList);
+//        System.out.println();
+//        System.out.println("Iterating over the ArrayList using while loop:");
+//        ArrayListIterator.arrayListIterationUsingWhile(arrayList);
 
 //        Fourth
-        System.out.println("Enter a text:");
-        String input3 = sc.nextLine();
-        System.out.println("Duplicate characters:");
-        Duplicates.findDuplicateCharacters(input3);
+//        System.out.println("Enter a text:");
+//        String input3 = sc.nextLine();
+//        System.out.println("Duplicate characters:");
+//        Duplicates.findDuplicateCharacters(input3);
 
 //        Fifth
-
+        String file = "src/main/resources/Test.xlsx";
+        ExcelReader.readExcel(file);
     }
 }
